ac5d24fe1af85d2b6c0a9b41c14f13dd06b91133
shubham88fru <shubham88fru@yahoo.com>
2025-02-01 08:08:33 -0700

    initial commit

---
+++ spring-framework-reactive/src/main/java/com/learning/springframeworkreactive/SpringFrameworkReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springframeworkreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkReactiveApplication.class, args);
+    }
+
+}

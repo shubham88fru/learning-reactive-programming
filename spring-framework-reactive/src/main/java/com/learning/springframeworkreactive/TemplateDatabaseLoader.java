1b1d13e9996a8f08c8e7cd58293c615b8bc103b2
shubham88fru <shubham88fru@yahoo.com>
2025-02-06 11:09:18 -0700

    local initial data to db and reactive cart repository

---
+++ spring-framework-reactive/src/main/java/com/learning/springframeworkreactive/TemplateDatabaseLoader.java
@@ -0,0 +1,17 @@
+package com.learning.springframeworkreactive;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TemplateDatabaseLoader {
+
+    CommandLineRunner initialize(MongoOperations mongoOperations) {
+        return args -> {
+            System.out.println("Initializing database...");
+            mongoOperations.save(new Item("Alarm clock", 19.99));
+            mongoOperations.save(new Item("Television", 24.99));
+        };
+    }
+}

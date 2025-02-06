59bc696198a96f4e1278d79d88fa1b8d80dcabe1
shubham88fru <shubham88fru@yahoo.com>
2025-02-06 11:16:45 -0700

    mark as spring bean

--- spring-framework-reactive/src/main/java/com/learning/springframeworkreactive/TemplateDatabaseLoader.java
+++ spring-framework-reactive/src/main/java/com/learning/springframeworkreactive/TemplateDatabaseLoader.java
@@ -1,12 +1,14 @@
 package com.learning.springframeworkreactive;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TemplateDatabaseLoader {
 
+    @Bean
     CommandLineRunner initialize(MongoOperations mongoOperations) {
         return args -> {
             System.out.println("Initializing database...");

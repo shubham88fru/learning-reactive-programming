c938f88b3d84f088dd9129cf4cb51d5926e9d654
shubham88fru <shubham88fru@yahoo.com>
2025-03-12 10:10:38 -0700

    initial commit for users module

---
+++ users/src/main/java/com/learning/reactive/web/users/infrastructure/H2ConsoleConfiguration.java
@@ -0,0 +1,27 @@
+package com.learning.reactive.web.users.infrastructure;
+
+import org.h2.tools.Server;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+
+import java.sql.SQLException;
+
+@Profile({"dev", "test"})
+@Configuration
+public class H2ConsoleConfiguration {
+    private Server server;
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void start() throws SQLException {
+        final String PORT = "8082";
+        this.server = Server.createWebServer("-webPort", PORT).start();
+    }
+
+    @EventListener(ContextClosedEvent.class)
+    public void stop() {
+        this.server.stop();
+    }
+}

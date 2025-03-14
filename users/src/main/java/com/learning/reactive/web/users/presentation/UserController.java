5b6c180118c50f2b485ea26ea0874fa45ccd6627
shubham88fru <shubham88fru@yahoo.com>
2025-03-14 09:27:43 -0700

    Presentation layer fully reactive

--- users/src/main/java/com/learning/reactive/web/users/presentation/UserController.java
+++ users/src/main/java/com/learning/reactive/web/users/presentation/UserController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -16,13 +17,18 @@ public class UserController {
     public Mono<ResponseEntity<UserRest>> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
 
         /*
-            ----------
-            IMPERATIVE
-            ----------
+            -------------------
+            IMPERATIVE/BLOCKING
+            -------------------
             UserRest userRest = new UserRest();
             return Mono.just(userRest);
          */
 
+        /*
+            -----------------------------------
+            DECLARATIVE/FUNCTIONAL/NON-BLOCKING
+            -----------------------------------
+         */
         return createUserRequest.map((request) -> {
             return new UserRest(UUID.randomUUID(), request.getFirstName(),
                     request.getLastName(), request.getEmail());
@@ -32,5 +38,15 @@ public class UserController {
     @GetMapping("/{userId}")
     public Mono<UserRest> getUser(@PathVariable UUID userId) {
         return Mono.just(new UserRest(userId, "Shubham", "Singh", "shubham@gmail.com"));
+    }
+
+    @GetMapping
+    public Flux<UserRest> getUsers(@RequestParam(value="offset", defaultValue = "0") int offset,
+                                   @RequestParam(value="limit", defaultValue = "50") int limit) {
+        return Flux.just(
+                new UserRest(UUID.randomUUID(), "Shubham", "Singh", "test@test.com"),
+                new UserRest(UUID.randomUUID(), "Shubham2", "Singh2", "test2@test.com"),
+                new UserRest(UUID.randomUUID(), "Shubham3", "Singh3", "test3@test.com")
+        );
     }
 }

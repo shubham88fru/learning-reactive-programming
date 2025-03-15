5e6badd438f9455b17c22ff3aff955736e30ebbc
shubham88fru <shubham88fru@yahoo.com>
2025-03-15 11:45:41 -0700

    add repository

---
+++ users/src/main/java/com/learning/reactive/web/users/data/UserEntity.java
@@ -0,0 +1,30 @@
+package com.learning.reactive.web.users.data;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Table(name="users")
+public class UserEntity {
+
+    @Id
+    private UUID id;
+
+    @Column("first_name")
+    private String firstName;
+
+    @Column("last_name")
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+
+}

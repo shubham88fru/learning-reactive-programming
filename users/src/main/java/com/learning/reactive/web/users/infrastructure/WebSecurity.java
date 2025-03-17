3aa3c63ec53037f3a585977569ff19de36c068ba
shubham88fru <shubham88fru@yahoo.com>
2025-03-17 08:56:11 -0700

    enable spring security

---
+++ users/src/main/java/com/learning/reactive/web/users/infrastructure/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.learning.reactive.web.users.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchanges ->
+                exchanges.pathMatchers(HttpMethod.POST, "/users").permitAll()
+                .anyExchange()
+                        .authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+}

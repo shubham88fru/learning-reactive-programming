e4043506105dac4b51956d9658358aff83a793dc
shubham88fru <shubham88fru@yahoo.com>
2025-03-18 09:02:32 -0700

    encrypt the password

--- users/src/main/java/com/learning/reactive/web/users/service/UserServiceImpl.java
+++ users/src/main/java/com/learning/reactive/web/users/service/UserServiceImpl.java
@@ -6,15 +6,18 @@ import com.learning.reactive.web.users.presentation.CreateUserRequest;
 import com.learning.reactive.web.users.presentation.UserRest;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.UUID;
 
@@ -23,6 +26,7 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Mono<UserRest> createUser(Mono<CreateUserRequest> createUserRequestMono) {
@@ -30,7 +34,8 @@ public class UserServiceImpl implements UserService {
 
 
         return createUserRequestMono
-                .map(this::convertToEntity)
+                //.map(this::convertToEntity)
+                .flatMap(this::convertToEntity)
                 .flatMap(userRepository::save) //flatmap flattens nested monos into a single Mono.
                 .map(this::convertToRest);
                 //.onErrorMap(DuplicateKeyException.class,
@@ -56,10 +61,31 @@ public class UserServiceImpl implements UserService {
         return userRepository.findAllBy(pageable).map(this::convertToRest);
     }
 
-    private UserEntity convertToEntity(CreateUserRequest createUserRequest) {
-        UserEntity userEntity = new UserEntity();
-        BeanUtils.copyProperties(createUserRequest, userEntity);
-        return userEntity;
+    private Mono<UserEntity> convertToEntity(CreateUserRequest createUserRequest) {
+
+        /*
+            Simply encrypting the password like below will be a CPU intensive
+            task and may be blocking.
+         */
+
+        /*
+            UserEntity userEntity = new UserEntity();
+            BeanUtils.copyProperties(createUserRequest, userEntity);
+            userEntity.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
+            return userEntity;
+         */
+
+        /*
+            Non blocking.
+         */
+        return Mono.fromCallable(
+                () -> {
+                    UserEntity userEntity = new UserEntity();
+                    BeanUtils.copyProperties(createUserRequest, userEntity);
+                    userEntity.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
+                    return userEntity;
+                }
+        ).subscribeOn(Schedulers.boundedElastic());
     }
 
     private UserRest convertToRest(UserEntity userEntity) {
